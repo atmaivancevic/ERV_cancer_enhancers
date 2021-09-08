b301db62734e31c68cf9b1f911d0cd4b967bda3c
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-09-08 11:21:10 -0600

    Update Figure1C_volcano_COAD.R

--- pancancer_epigenomic_analysis/Figure1C_volcano_COAD.R
+++ pancancer_epigenomic_analysis/Figure1C_volcano_COAD.R
@@ -1,68 +1,58 @@
-source("https://bioconductor.org/biocLite.R")
 library("ggplot2")
-biocLite("ggrepel")
 library("ggrepel")
 
-### Read in the output from featureCounts.
-countdata <- read.table("COAD_minus_Roadmap167.tab.volcano", header=TRUE, sep="\t")
-head(countdata)
+# Read in the table
+COAD_minus_Roadmap <- read.table("COAD_minus_Roadmap.tab.volcano", header=TRUE, sep="\t")
+head(COAD_minus_Roadmap)
 
 # Set first column to be the rownames
-rownames(countdata) <- countdata[,1]
-head(countdata)
+rownames(COAD_minus_Roadmap) <- COAD_minus_Roadmap[,1]
+head(COAD_minus_Roadmap)
 
-### Remove the first column
-countdata <- countdata[ ,2:ncol(countdata)]
-head(countdata)
+# Remove the first column
+COAD_minus_Roadmap <- COAD_minus_Roadmap[ ,2:ncol(COAD_minus_Roadmap)]
+head(COAD_minus_Roadmap)
 
-# volcano plot
-fishers_two_tail_pval <- countdata[,c(2)]
+# Plot histograms & volcano plot
+fishers_two_tail_pval <- COAD_minus_Roadmap[,c(2)]
 head(fishers_two_tail_pval)
 hist(fishers_two_tail_pval)
 hist(-log10(fishers_two_tail_pval))
 
-odds_ratio <- countdata[,c(1)]
-head(odds_ratio)
+odds_ratio <- COAD_minus_Roadmap[,c(1)]
 hist(odds_ratio)
 
-new <- countdata[,c(1,2)]
-head(new)
-new = as.data.frame(new)
-new$probename <- rownames(new)
-head(new)
+COAD_simple <- COAD_minus_Roadmap[,c(1,2)]
+head(COAD_simple)
+COAD_simple = as.data.frame(COAD_simple)
+COAD_simple$probename <- rownames(COAD_simple)
+head(COAD_simple)
 
-new["LTR10F", ]
-new["LTR10A", ]
+# This last subset will be for adding labels to the most enriched/depleted TEs
+labelsEnriched = subset(COAD_simple, fishers_two_tail_pval<0.00000000000000000000000005 & odds_ratio>30)
+nrow(labelsEnriched)
+rownames(labelsEnriched)
 
-# Separate data into subsets
-value1 = subset(new, fishers_two_tail_pval<0.05)
-nrow(value1)
-head(value1)
+labelsDepleted = subset(COAD_simple, fishers_two_tail_pval<0.0000000000000000000005 & odds_ratio<0.25)
+nrow(labelsDepleted)
+rownames(labelsDepleted)
 
-value2 = subset(new, odds_ratio>1)
-nrow(value2)
-value2
-
-value3 = subset(new, fishers_two_tail_pval<0.05 & odds_ratio>1)
-dim(value3)
-value3
-
-# plot it
-ggplot(new, aes(log2(odds_ratio), -log10(fishers_two_tail_pval)), colour="grey") +
-  scale_color_discrete(name = 'Labels') +
+# Plot it
+ggplot(COAD_simple, aes(log2(odds_ratio),-log10(fishers_two_tail_pval)), colour="grey") +
+  scale_color_discrete(name='Labels') +
   theme_classic() +
-  labs(y="-Log10 P-value", x = "Log2 Odds Ratio") +
-  # Set all dots color to grey
-  geom_point(data=new, colour = "grey", size = 1) +
-  # If pvalue<0.05, change dot color to green
-  geom_point(data=new[which(new $odds_ratio >3 & new $fishers_two_tail_pval <0.005),], colour = "red", size=1) +
-  geom_point(data=new[which(new $odds_ratio <(1/3) & new $fishers_two_tail_pval <0.005),], colour = "blue", size=1) +
-  geom_text_repel(data = value4, mapping = aes(log2(odds_ratio), -log10(fishers_two_tail_pval), label = rownames(value4)), force = 1) +
-  geom_text_repel(data = value5, mapping = aes(log2(odds_ratio), -log10(fishers_two_tail_pval), label = rownames(value5)),force = 1) +
-  xlim(-8.2, 8.2) +
-  #ylim(0, 66) +
-  theme(text = element_text(size=11, family="Helvetica"), 
-        axis.text.x = element_text(size = 10, family="Helvetica"),
-        axis.text.y = element_text(size = 10, family="Helvetica"),
-        axis.title.x = element_text(size = 10, family="Helvetica"),
-        axis.title.y = element_text(size = 10, family="Helvetica"))
+  labs(y="-Log10 P-value", x="Log2 Odds Ratio") +
+  # Set all dots to grey
+  geom_point(data=COAD_simple, colour="grey", size=1) +
+  # Change dot colour to red (enriched) or blue (depleted) based on odds ratio and pval cutoffs
+  geom_point(data=COAD_simple[which(COAD_simple$odds_ratio>3 & COAD_simple$fishers_two_tail_pval<0.005),], colour="red", size=1) +
+  geom_point(data=COAD_simple[which(COAD_simple$odds_ratio<(1/3) & COAD_simple$fishers_two_tail_pval<0.005),], colour="blue", size=1) +
+  # Add labels to the most enriched/depleted points
+  geom_text_repel(data=labelsEnriched, mapping=aes(log2(odds_ratio),-log10(fishers_two_tail_pval),label=rownames(labelsEnriched)), force=1) +
+  geom_text_repel(data=labelsDepleted, mapping=aes(log2(odds_ratio),-log10(fishers_two_tail_pval),label=rownames(labelsDepleted)), force=1) +
+  xlim(-8.2,8.2) +
+  theme(text=element_text(size=11,family="Helvetica"), 
+        axis.text.x=element_text(size=10,family="Helvetica"),
+        axis.text.y=element_text(size=10,family="Helvetica"),
+        axis.title.x=element_text(size=10,family="Helvetica"),
+        axis.title.y=element_text(size=10,family="Helvetica"))
