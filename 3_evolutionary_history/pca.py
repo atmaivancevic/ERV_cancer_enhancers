6ddae7b27e73e004dd54ef0bda9d21c50b565617
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-09-10 14:38:03 -0600

    Update pca.py

--- 3_evolutionary_history/pca.py
+++ 3_evolutionary_history/pca.py
@@ -1,1 +1,19 @@
-placeholder
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('jalview_pca_transformed_points_allLTR10_min200_alignment.csv', sep=',')
+#print(df)
+
+fig = px.scatter(df, x="PC1", y="PC2",
+                color="subfamily",
+                color_discrete_sequence=["red", "green", "blue", "purple", "magenta","orange","grey"],
+                hover_data=df.columns,
+                title="Clusterf*ck",
+                marginal_y="violin",
+                marginal_x="histogram",
+                )
+
+fig.for_each_trace(lambda t: t.update(name=t.name.replace("=",": ")))
+            
+fig.show()
+#py.plot(fig, filename = 'clusterfck', auto_open=True) 
