90472b9263059b51292a8b989a6a5beb015e6808
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-09-13 11:16:51 -0600

    Update add_gene_names.sh

--- rnaseq/add_gene_names.sh
+++ rnaseq/add_gene_names.sh
@@ -8,7 +8,9 @@ cat featureCounts.txt \
 | grep -v "Geneid" \
 > featureCounts_noHeader.txt
 
-# add gene names by matching gene ids
+# Add gene names by matching gene ids
 awk 'NR==FNR{c[$1]++;next};c[$1] > 0' featureCounts_noHeader.txt gencode34_geneid_genename.txt \
 | paste - featureCounts_noHeader.txt \
+| awk '{print $1 "\t" $2 "\t" $3 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\t" $13 "\t" $14 "\t" $15}' \
+| sed '1iGeneid\tGeneName\tGeneDesc\tLength\tCobi_24_1_S54\tCobi_24_2_S55\tTNF_24_1_S58\tTNF_24_2_S59\tUT_24_1_S52\tUT_24_2_S53' \
 > featureCounts_withGeneNames.txt 
