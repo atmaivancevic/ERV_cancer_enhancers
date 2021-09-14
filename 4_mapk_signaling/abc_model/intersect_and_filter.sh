b25da178e460442b299db23e97bd1e7059131107
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-09-14 15:05:46 -0600

    Update intersect_and_filter.sh

--- 4_mapk_signaling/abc_model/intersect_and_filter.sh
+++ 4_mapk_signaling/abc_model/intersect_and_filter.sh
@@ -18,10 +18,3 @@ cat EnhancerPredictionsAllPutativehg38_intersect_LTR10AF.bed \
 | sort -nrk5 \
 > EnhancerPredictionsAllPutativehg38_intersect_LTR10AF_scoreOver0.001.bed
 # 3583
-
-########## UP TO HERE! 
-
-
-# Intersect with H3K27ac-marked LTR10A/F only
-bedtools intersect -a EnhancerPredictionsAllPutativehg38_intersect_LTR10AF_scoreOver0.001_unique.bed -b normalized_counts_H3K27ac_CnR_modified.tmp -loj | grep -v "\-1"  | awk '{if ($8!="0") print}' | awk '{print $1 "\t" $2 "\t" $3 "\t" $4}' > 98_LTR10_derived_enhancers.bed
-
