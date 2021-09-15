ac3b525f40052ef50f83f71d2d1d8f4c105d2fc6
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-09-15 14:04:30 -0600

    Update deseq2_TEs.R

--- rnaseq/deseq2_TEs.R
+++ rnaseq/deseq2_TEs.R
@@ -6,7 +6,7 @@ suppressPackageStartupMessages(library(DESeq2))
 rm(list = ls())
 
 # Read table
-TEdata <- read.table("Cobi_24hr_bothReps_strandedNo_TEsonly.counttab",header=T,row.names=1)
+TEdata <- read.table("TNF_24hr_bothReps_strandedNo_TEsonly.counttab",header=T,row.names=1)
 head(TEdata)
 
 groups <- factor(c(rep("TGroup",2),rep("CGroup",2)))
@@ -23,33 +23,33 @@ dds <- DESeq(dds)
 # Save the count tables
 
 # Raw counts
-raw_counts_tetranscripts_cobi_24hr = counts(dds)
-head(raw_counts_tetranscripts_cobi_24hr)
-write.table(raw_counts_tetranscripts_cobi_24hr, file="raw_counts_tetranscripts_cobi_24hr.tab", quote = FALSE, row.names = TRUE, sep = "\t")
+raw_counts_tetranscripts_tnf_24hr = counts(dds)
+head(raw_counts_tetranscripts_tnf_24hr)
+write.table(raw_counts_tetranscripts_tnf_24hr, file="raw_counts_tetranscripts_tnf_24hr.tab", quote = FALSE, row.names = TRUE, sep = "\t")
 
 # Normalized counts
-normalized_counts_tetranscripts_cobi_24hr <- counts(dds, normalized=TRUE)
-tail(normalized_counts_tetranscripts_cobi_24hr)
-write.table(normalized_counts_tetranscripts_cobi_24hr, file="normalized_counts_tetranscripts_cobi_24hr.tab", quote = FALSE, row.names = TRUE, sep = "\t")
+normalized_counts_tetranscripts_tnf_24hr <- counts(dds, normalized=TRUE)
+tail(normalized_counts_tetranscripts_tnf_24hr)
+write.table(normalized_counts_tetranscripts_tnf_24hr, file="normalized_counts_tetranscripts_tnf_24hr.tab", quote = FALSE, row.names = TRUE, sep = "\t")
 
 # Save the deseq2 output
 
 resultsNames(dds)
 #BiocManager::install("apeglm")
 #library(apeglm)
-deseq2_results_tetranscripts_cobi_vs_ctrl <- lfcShrink(dds, coef="groups_TGroup_vs_CGroup",type="apeglm")
-head(deseq2_results_tetranscripts_cobi_vs_ctrl)
-dim(deseq2_results_tetranscripts_cobi_vs_ctrl)
-#1023
+deseq2_results_tetranscripts_tnf_vs_ctrl <- lfcShrink(dds, coef="groups_TGroup_vs_CGroup",type="apeglm")
+head(deseq2_results_tetranscripts_tnf_vs_ctrl)
+dim(deseq2_results_tetranscripts_tnf_vs_ctrl)
+#1021
 
 # Remove NAs (e.g. due to no reads in all samples)
-deseq2_results_tetranscripts_cobi_vs_ctrl <- na.omit(deseq2_results_tetranscripts_cobi_vs_ctrl)
-dim(deseq2_results_tetranscripts_cobi_vs_ctrl)
-#587
+deseq2_results_tetranscripts_tnf_vs_ctrl <- na.omit(deseq2_results_tetranscripts_tnf_vs_ctrl)
+dim(deseq2_results_tetranscripts_tnf_vs_ctrl)
+#1021
 
 # Sort by lowest padj value
-deseq2_results_tetranscripts_cobi_vs_ctrl <- deseq2_results_tetranscripts_cobi_vs_ctrl[order(deseq2_results_tetranscripts_cobi_vs_ctrl$padj), ]
-head(deseq2_results_tetranscripts_cobi_vs_ctrl, 10)
+deseq2_results_tetranscripts_tnf_vs_ctrl <- deseq2_results_tetranscripts_tnf_vs_ctrl[order(deseq2_results_tetranscripts_tnf_vs_ctrl$padj), ]
+head(deseq2_results_tetranscripts_tnf_vs_ctrl, 10)
 
 # Save deseq-normalized table sorted by padj
-write.table(deseq2_results_tetranscripts_cobi_vs_ctrl, file="deseq2_results_tetranscripts_cobi_vs_ctrl.tab", quote = FALSE, row.names = TRUE, sep = "\t")
+write.table(deseq2_results_tetranscripts_tnf_vs_ctrl, file="deseq2_results_tetranscripts_tnf_vs_ctrl.tab", quote = FALSE, row.names = TRUE, sep = "\t")
