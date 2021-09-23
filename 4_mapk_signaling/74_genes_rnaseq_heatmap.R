64d5ad536b56ff9c8e0ea338db5d01298c6f7408
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-09-23 14:28:00 -0600

    Update 74_genes_rnaseq_heatmap.R

--- 4_mapk_signaling/74_genes_rnaseq_heatmap.R
+++ 4_mapk_signaling/74_genes_rnaseq_heatmap.R
@@ -1,3 +1,7 @@
+# Clear workspace
+rm(list = ls())
+
+# Read table
 norm_counts_74_genes <- read.table("74_genes_normalized_counts_rnaseq.tab", sep="\t", header = TRUE)
 head(norm_counts_74_genes)
 
@@ -9,12 +13,13 @@ head(norm_counts_74_genes)
 norm_counts_74_genes <- norm_counts_74_genes[ ,2:ncol(norm_counts_74_genes)]
 head(norm_counts_74_genes)
 
+# Reorder columns
 norm_counts_74_genes <- norm_counts_74_genes[ ,c("Cobi_24h_1", "Cobi_24h_2", "UT_24h_1", "UT_24h_2", "TNF_24h_1", "TNF_24h_2")]
 head(norm_counts_74_genes)
 
 norm_counts_74_genes = as.data.frame(norm_counts_74_genes)
 
-### Run pheatmap
+### Make a heatmap
 pheatmap(norm_counts_74_genes, color = colorRampPalette(rev(brewer.pal(n = 7, name ="RdYlBu")))(100),
          cluster_rows = T, clustering_distance_rows = "manhattan", show_rownames=T, fontsize = 10,
          fontsize_row = 10, height=20, cluster_cols = F, scale="row")
