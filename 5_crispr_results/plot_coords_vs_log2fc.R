2c70bcb6dd88b66b5960475e8f5b359b80291950
Atma M Ivancevic <atma.ivancevic@gmail.com>
2021-10-08 15:17:17 -0600

    Update plot_coords_vs_log2fc.R

--- 5_crispr_results/plot_coords_vs_log2fc.R
+++ 5_crispr_results/plot_coords_vs_log2fc.R
@@ -32,7 +32,7 @@ ggplot(deseq_with_gene_coords, aes(geneStart, log2FoldChange), colour="grey") +
   scale_color_discrete(name = 'Labels') +
   theme_bw() + 
   geom_hline(aes(yintercept = 0), size=0.2) +
-  ggtitle("LTR10.ATG12i") +
+  ggtitle("CRISPRi: LTR10.ATG12 vs Control") +
   theme(plot.title = element_text(hjust = 0.5)) +
   # change plot margins (order is top, right, bottom, left)
   theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm")) +
@@ -41,8 +41,11 @@ ggplot(deseq_with_gene_coords, aes(geneStart, log2FoldChange), colour="grey") +
   # Set all dots color to grey
   geom_point(data=deseq_with_gene_coords[which(deseq_with_gene_coords$geneChr=="chr5"),], colour = "grey", size = 1.5) + 
   # add annotation for the targeted LTR10 element
-  annotate("rect", xmin=115928578-500000, xmax=115928578+500000, ymin=-0.12, ymax=0.12, alpha=0.99, fill="black") +
-  annotate("text", x=115928578, y=0.4, label = "Targeted LTR10", size=4.5, family="Helvetica") +
+  annotate("rect", xmin=115928578-300000, xmax=115928578+300000, ymin=-0.09, ymax=0.09, alpha=0.99, fill="black") +
+  annotate("text", x=115928578, y=0.4, label = "LTR10.ATG12", size=4.5, family="Helvetica") +
+  # add annotation for the scale bar
+  annotate("rect", xmin=115928578-5000000, xmax=115928578-5000000+1500000, ymin=1.3, ymax=1.32, alpha=0.99, fill="black") +
+  annotate("text", x=115928578-5000000+750000, y=1.1, label = "1.5Mb", size=4.5, family="Helvetica") +
   # If pvalue<0.05, change dot color to blue
   geom_point(data=deseq_with_gene_coords[which(deseq_with_gene_coords$geneChr=="chr5" & deseq_with_gene_coords $padj <0.05 & deseq_with_gene_coords $log2FoldChange>0),],  colour = "blue", size = 3) +
   # Highlight in red genes within 1Mb of targeted LTR10 (still requiring pval<0.05 and log2fc<0)
@@ -51,10 +54,10 @@ ggplot(deseq_with_gene_coords, aes(geneStart, log2FoldChange), colour="grey") +
   geom_text_repel(data=deseq_with_gene_coords[which(deseq_with_gene_coords$geneChr=="chr5" & deseq_with_gene_coords$padj<0.05 & deseq_with_gene_coords$log2FoldChange<0 & deseq_with_gene_coords $geneStart>(115928578-1500000) & deseq_with_gene_coords $geneStart<(115928578+1500000)),], mapping = aes(geneStart, log2FoldChange, label = rownames(deseq_with_gene_coords[which(deseq_with_gene_coords$geneChr=="chr5" & deseq_with_gene_coords $padj<0.05 & deseq_with_gene_coords $log2FoldChange<0 & deseq_with_gene_coords $geneStart>(115928578-1500000) & deseq_with_gene_coords $geneStart<(115928578+1500000)),])),size=4,force=2) +
   # Set the viewing window to be 10Mb centered around LTR10
   scale_x_continuous(labels = scales::comma, limits = c(115928578-5000000,115928578+5000000)) +
-  scale_y_continuous(expand = c(0,0),  limits = c(-3.5,3.5)) +
+  scale_y_continuous(expand = c(0,0),  limits = c(-3.5,1.5)) +
   theme(text=element_text(size=12,family="Helvetica"))
 
-# export pdf 5.5 by 6 inches
+# export pdf 4.5 by 6 inches
 
 # Coordinates of enhancers and genes:
 # LTR10.ATG12 enhancer at chr5 115928578
@@ -67,3 +70,4 @@ ggplot(deseq_with_gene_coords, aes(geneStart, log2FoldChange), colour="grey") +
 # ATG12 gene at chr5 115828199
 # FOSL1 gene at chr11 65892048
 # JUN gene at chr1 58780790
+
